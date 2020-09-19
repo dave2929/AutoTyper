5e05caebefdb5e35ba51126afaaea2bcd4f7c68f
David He <davidhe2929@gmail.com>
2020-09-18 20:42:04 -0400

    hotkey and tray

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const win = require("electron").remote.getCurrentWindow();
 const ipcRenderer = require("electron").ipcRenderer;
-let el = document.getElementsByClassName("text");
+var el = document.getElementsByClassName("text");
 for (let i = 0; i < el.length; i++) {
   el[i].addEventListener("click", () => {
     ipcRenderer.send("type", el[i].innerHTML);
--- main.js
+++ main.js
@@ -1,8 +1,17 @@
-const { app, BrowserWindow, ipcMain } = require("electron");
+const {
+  app,
+  BrowserWindow,
+  ipcMain,
+  globalShortcut,
+  Tray,
+  Menu,
+} = require("electron");
 const robot = require("robotjs");
-// robot.setKeyboardDelay(1);
-let floatingWindow;
-let settingsWindow;
+robot.setKeyboardDelay(1);
+var floatingWindow;
+var settingsWindow;
+var tray;
+var floatingWindowShow = true;
 function createWindow() {
   floatingWindow = new BrowserWindow({
     width: 250,
@@ -20,27 +29,72 @@ function createWindow() {
     // opacity: 0.5,
   });
   settingsWindow = new BrowserWindow({
-    frame: false,
+    width: 800,
+    height: 600,
+    titleBarStyle: "hide",
+    icon: "favicon.ico",
   });
   floatingWindow.loadFile("floating_window.html");
   floatingWindow.setAlwaysOnTop(true, "floating");
-  // floatingWindow.setIgnoreMouseEvents(true);
+  settingsWindow.loadFile("settings_window.html");
   // floatingWindow.webContents.openDevTools();
 
   floatingWindow.show();
+  floatingWindowShow = true;
   settingsWindow.show();
 
   // close
-  settingsWindow.on("closed", () => {
-    floatingWindow.close();
+  settingsWindow.on("close", (event) => {
+    if (!app.isQuiting) {
+      event.preventDefault();
+      settingsWindow.hide();
+    }
   });
 }
+
+function createTray() {
+  tray = new Tray("favicon.ico");
+  let template = [
+    {
+      label: "Show",
+      click: () => {
+        settingsWindow.show();
+      },
+    },
+    {
+      label: "Exit",
+      click: () => {
+        app.isQuiting = true;
+        app.quit();
+      },
+    },
+  ];
+  const contextMenu = Menu.buildFromTemplate(template);
+  tray.on("click", (event) => {
+    settingsWindow.show();
+  });
+  tray.setToolTip("AutoTyper");
+  tray.setContextMenu(contextMenu);
+}
+
 ipcMain.on("type", (event, msg) => {
-  // console.log(msg);
+  robot.keyTap("enter");
   robot.typeStringDelayed(msg, 99999);
   robot.keyTap("enter");
 });
-app.on("ready", createWindow);
+app.on("ready", () => {
+  createWindow();
+  createTray();
+  globalShortcut.register("`", () => {
+    if (floatingWindowShow == true) {
+      floatingWindow.hide();
+      floatingWindowShow = false;
+    } else {
+      floatingWindow.show();
+      floatingWindowShow = true;
+    }
+  });
+});
 
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") {
